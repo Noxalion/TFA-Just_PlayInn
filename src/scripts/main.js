6196f2d949cf695f61b2c728916d9e14251a7896
bastien gathoye <bastiengathoye@gmail.com>
2023-05-24 22:24:38 +0200

    début de mise en forme des boutons, créations des espaces pour les sections, mise en place du burgermenu et début de mise en forme des zones de détails

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
+"use strict";
 
+//burgermenu
+let burgerMenu = document.querySelector(".menu__btn");
+let menu = document.querySelector(".menu__content");
+if (burgerMenu && menu) {
+    burgerMenu.addEventListener("click", burgerStatus);
+}
+
+function burgerStatus(){
+    menu.classList.toggle("open");
+}
+    //cette partie, c'est pour fermer le menu si on est dans une one-page
+let anchor = document.querySelectorAll(".anchor");
+if (anchor) {
+    for (let i = 0; i < anchor.length; i++) {
+        anchor[i].addEventListener("click", burgerStatus);
+    }
+}

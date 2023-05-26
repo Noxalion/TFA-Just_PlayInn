e320ae143b838265ea06caee431a2877b21dfc66
bastien gathoye <bastiengathoye@gmail.com>
2023-05-26 18:10:08 +0200

    avancement sur le slider, a mis parcours; save car change de boilderplate, erreur avec npm comprends pas donc essai de changement pour npm

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
 "use strict";
+
+const { check } = require("yargs");
+
 let body = document.querySelector(".body");
 
 //burgermenu
@@ -49,5 +52,50 @@ function zoneDetailOpen(el){
 function stopScrollMobile(){
     if (screen.width < 1000) {
         body.classList.toggle("blockScroll");
+    }
+}
+
+//slider
+const buttons = {
+	prev: document.querySelector(".btn--left"),
+	next: document.querySelector(".btn--right"),
+};
+const cardsContainerEl = document.querySelector(".cards");
+
+buttons.next.addEventListener("click", () => swapCards("right"));
+
+buttons.prev.addEventListener("click", () => swapCards("left"));
+
+function swapCards(direction) {
+	const currentCardEl = cardsContainerEl.querySelector(".current--card");
+	const previousCardEl = cardsContainerEl.querySelector(".previous--card");
+	const nextCardEl = cardsContainerEl.querySelector(".next--card");
+
+	swapCardsClass();
+    checkCardsSize();
+
+	function swapCardsClass() {
+		currentCardEl.classList.remove("current--card");
+		previousCardEl.classList.remove("previous--card");
+		nextCardEl.classList.remove("next--card");
+
+		if (direction === "right") {
+			currentCardEl.classList.add("previous--card");
+			previousCardEl.classList.add("next--card");
+			nextCardEl.classList.add("current--card");
+
+		} else if (direction === "left") {
+			currentCardEl.classList.add("next--card");
+			previousCardEl.classList.add("current--card");
+			nextCardEl.classList.add("previous--card");
+		}
+	}
+
+    function checkCardsSize() {
+        if (currentCardEl.classList.contains("cards__card--smaller")) {
+            cardsContainerEl.classList.add("cards--smaller");
+        }else{
+            cardsContainerEl.classList.remove("cards--smaller");
+        }
     }
 }
